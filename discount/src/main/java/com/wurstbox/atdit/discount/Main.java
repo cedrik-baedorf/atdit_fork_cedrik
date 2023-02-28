ed7987aa4cf0911dda87956125477be3bb627c74
Patrick <pet@wurstbox.com>
2023-02-28 17:06:24 +0100

    Implementing discount module - only one class and method - contract not compliant to specification - missing datasource abstraction - database connection not in config file - bad variable names - unused fields - redundant initialization - logging only to console - no user interface - ignored exception - no unit tests - contains bugs

---
+++ discount/src/main/java/com/wurstbox/atdit/discount/Main.java
@@ -0,0 +1,51 @@
+package com.wurstbox.atdit.discount;
+
+import java.sql.*;
+
+public class Main {
+  private static final String url = "jdbc:mariadb://localhost:3306/atdit";
+  private static final String user = "atdit_user";
+  private static final String password = "LaXI8i3abe1aluYoRAC3nAhIyiJ6hi";
+
+  public double computeDiscount( double base, int customer ) {
+    Connection c = null;
+    PreparedStatement s = null;
+    double result = 0;
+    ResultSet r = null;
+
+    try {
+      c = DriverManager.getConnection( url, user, password );
+
+      s = c.prepareStatement(
+          """
+            SELECT d.discount_id, d.discount, d.discount_text
+              FROM discount AS d
+                INNER JOIN customer_discount cd ON d.discount_id = cd.discount_id
+              WHERE customer_id = ?
+          """ );
+      s.setInt( 1, customer );
+      r = s.executeQuery();
+
+      while( r.next() ) {
+        int id = r.getInt( 1 );
+        double val1 = r.getDouble( 2 ); // discount_text
+        String val2 = r.getString( 3 ); // discount
+
+        result = base * val1 / 100;
+        System.out.println( "Granted " + val1 + " % " + val2 + " discount: " + result + " €" );
+      }
+
+      r.close();
+      s.close();
+      c.close();
+    }
+    catch( Exception sql ) { }
+
+    return result;
+  }
+
+  public static void main( String[] args ) {
+    Main m = new Main();
+    System.out.println( m.computeDiscount( 250, 2 ) );
+  }
+}
