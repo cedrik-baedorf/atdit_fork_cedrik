2c8ff3dc8ee055169ac705fff678617501330fac
Patrick <pet@wurstbox.com>
2023-02-28 19:24:38 +0100

    Correction 3 - Unit Tests adopted to new contract

--- discount/src/test/java/com/wurstbox/atdit/discount/DiscountComputerImplementationTest.java
+++ discount/src/test/java/com/wurstbox/atdit/discount/DiscountComputerImplementationTest.java
@@ -3,20 +3,26 @@ package com.wurstbox.atdit.discount;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
+import java.util.ArrayList;
+
+public class DiscountComputerImplementationTest {
 
   /**
    This method tests whether a customer who does not receive any discount gets no discount.<br/>
    Customer 0 is granted no discount.<br/>
    Tests: {@link DiscountComputerImplementation#computeDiscount(double, int)}<br/>
    Input: base = 100, customer = 0<br/>
-   Expected Output: 0
+   Expected Output: List with one entry (Text | percentage | amount):
+   <ol>
+   <li>"Aggregate", 0, 0</li>
+   </ol>
    */
   @Test
   public void noDiscount() {
     //assemble
     var cut = new DiscountComputerImplementation();
-    var expected = 0d;
+    var expected = new ArrayList<Discount>( 1 );
+    expected.add( new Discount( "Aggregate", 0, 0 ) );
 
     //act
     var actual = cut.computeDiscount( 100, 0 );
@@ -30,13 +36,19 @@ public class MainTest {
    Customer 1 is granted one single discount of 19%.<br/>
    Tests: {@link DiscountComputerImplementation#computeDiscount(double, int)}<br/>
    Input: base = 100, customer = 1<br/>
-   Expected Output: 19
+   Expected Output: List with two entries (Text | percentage | amount):
+   <ol>
+   <li>"Aggregate", 19, 19</li>
+   <li>"geschenkte Mehrwertsteuer", 19, 19</li>
+   </ol>
    */
   @Test
   public void singleDiscount() {
     //assemble
     var cut = new DiscountComputerImplementation();
-    var expected = 19d;
+    var expected = new ArrayList<Discount>( 2 );
+    expected.add( new Discount( "Aggregate", 19, 19 ) );
+    expected.add( new Discount( "geschenkte Mehrwertsteuer", 19, 19 ) );
 
     //act
     var actual = cut.computeDiscount( 100, 1 );
@@ -50,13 +62,23 @@ public class MainTest {
    Customer 2 is granted three discounts of 3, 5, and 19%, that is 27% altogether.<br/>
    Tests: {@link DiscountComputerImplementation#computeDiscount(double, int)}<br/>
    Input: base = 100, customer = 2<br/>
-   Expected Output: 27
+   Expected Output: List with four entries (Text | percentage | amount):
+   <ol>
+   <li>"Aggregate", 19, 19</li>
+   <li>"Semesterstart", 5, 5</li>
+   <li>"Aktionswochen", 3, 3</li>
+   <li>"geschenkte Mehrwertsteuer", 19, 19</li>
+   </ol>
    */
   @Test
   public void multipleDiscounts() {
     //assemble
     var cut = new DiscountComputerImplementation();
-    var expected = 27d;
+    var expected = new ArrayList<Discount>( 4 );
+    expected.add( new Discount( "Aggregate", 27, 27 ) );
+    expected.add( new Discount( "Semesterstart", 5, 5 ) );
+    expected.add( new Discount( "Aktionswochen", 3, 3 ) );
+    expected.add( new Discount( "geschenkte Mehrwertsteuer", 19, 19 ) );
 
     //act
     var actual = cut.computeDiscount( 100, 2 );
