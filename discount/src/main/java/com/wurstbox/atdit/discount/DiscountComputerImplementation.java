edf8da8d734c39ec4dd3031c46182cbaae9712e8
Patrick <pet@wurstbox.com>
2023-03-04 12:38:41 +0100

    Correction 4 - Refactoring - Extracting methods making the code more modular

--- discount/src/main/java/com/wurstbox/atdit/discount/DiscountComputerImplementation.java
+++ discount/src/main/java/com/wurstbox/atdit/discount/DiscountComputerImplementation.java
@@ -1,88 +1,122 @@
 package com.wurstbox.atdit.discount;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 
 /**
  Current Implementation for discount computation (see {@link DiscountComputer}).
  */
 public class DiscountComputerImplementation implements DiscountComputer {
-  private static final String url = "jdbc:mariadb://localhost:3306/atdit";
-  private static final String user = "atdit_user";
-  private static final String password = "LaXI8i3abe1aluYoRAC3nAhIyiJ6hi";
-
   DiscountComputerImplementation() {
   }
 
   /**
    @implSpec To compute the discount, the customer number is looked up in database. Associated discounts are retrieved and
    computed according to the rules defined in {@link DiscountComputer}.
-   The database fulfill following requirements:
+   A database must be available and running. Connection details must be provided in "db.properties". Required properties are:
    <ul>
-   <li>MariaDB</li>
-   <li>Run on localhost:3306</li>
-   <li>Schema: atdit</li>
-   <li>user: atdit_user</li>
-   <li>have a certain password that we can name here for it is written in the code anyway: LaXI8i3abe1aluYoRAC3nAhIyiJ6hi</li>
+   <li>url: database connection string except for user and passwort</li>
+   <li>user: db user name</li>
+   <li>password: db password</li>
    </ul>
    */
   @Override
   public List<Discount> computeDiscount( double base, int customer ) {
     List<Discount> result = new ArrayList<>();
 
-    Connection c = null;
-    PreparedStatement s = null;
-    ResultSet r = null;
-
-    try {
-      c = DriverManager.getConnection( url, user, password );
-
-      s = c.prepareStatement(
-          """
-          SELECT d.discount_id, d.discount, d.discount_text
-            FROM discount AS d
-              INNER JOIN customer_discount cd ON d.discount_id = cd.discount_id
-            WHERE customer_id = ?
-                                """ );
-      s.setInt( 1, customer );
-      r = s.executeQuery();
-
-      while( r.next() ) {
-        double discountPercentage = r.getDouble( 2 ); // discount_text
-        String discountText = r.getString( 3 ); // discount
-        double discountValue = base * discountPercentage / 100;
-
-        Discount discount = new Discount(
-            discountText,
-            discountPercentage,
-            discountValue );
-
-        System.out.println(
-            "Granted " + discountPercentage + " % " +
-            discountText + " discount: " + discountValue + " €" );
-        result.add( discount );
-      }
-
-      Discount aggregate = new Discount( "Aggregate", 0, 0 );
-      for( Discount d : result ) {
-        aggregate = new Discount(
-            aggregate.description(),
-            aggregate.percentage() + d.percentage(),
-            aggregate.amount() + d.amount() );
-      }
-      result.add( 0, aggregate );
-
-      r.close();
-      s.close();
-      c.close();
-    }
-    catch( Exception sql ) { }
+    var dbData = getDiscountsFromDB( customer );
+    convertDBDataToResult( base, result, dbData );
+    addAggregateDiscounts( result );
 
     return result;
   }
+
+  private void addAggregateDiscounts( List<Discount> result ) {
+    Discount aggregate = new Discount( "Aggregate", 0, 0 );
+    for( Discount d : result ) {
+      aggregate = new Discount(
+          aggregate.description(),
+          aggregate.percentage() + d.percentage(),
+          aggregate.amount() + d.amount() );
+    }
+    result.add( 0, aggregate );
+  }
+
+  private void convertDBDataToResult( double base, List<Discount> result, List<DiscountDB> dbData ) {
+    for( DiscountDB discountDB : dbData ) {
+      double discountValue = base * discountDB.discount() / 100;
+      Discount discount = new Discount(
+          discountDB.discountText(),
+          discountDB.discount(),
+          discountValue );
+      result.add( discount );
+    }
+  }
+
+  private List<DiscountDB> getDiscountsFromDB( int customer ) {
+    List<DiscountDB> result = new ArrayList<>();
+
+    Properties dbAccessProperties = getDbAccessProperties();
+
+    String url = dbAccessProperties.getProperty( "url" );
+    String user = dbAccessProperties.getProperty( "user" );
+    String password = dbAccessProperties.getProperty( "password" );
+
+    try( Connection connection = getConnection( url, user, password );
+         PreparedStatement statement = prepareStatement( connection, customer );
+         ResultSet dbQueryResult = statement.executeQuery() ) {
+      fillResultList( result, dbQueryResult );
+    }
+    catch( SQLException e ) {
+      throw new RuntimeException( "database access failed" );
+    }
+
+    return result;
+  }
+
+  private void fillResultList( List<DiscountDB> result, ResultSet dbQueryResult ) throws SQLException {
+    while( dbQueryResult.next() ) {
+      int discountID = dbQueryResult.getInt( "discount_id" );
+      double discount = dbQueryResult.getDouble( "discount" );
+      String discountText = dbQueryResult.getString( "discount_text" );
+
+      DiscountDB resultLine = new DiscountDB( discountID, discount, discountText );
+      result.add( resultLine );
+    }
+  }
+
+  private PreparedStatement prepareStatement( Connection connection, int customer ) throws SQLException {
+    PreparedStatement result = connection.prepareStatement(
+        """
+        SELECT d.discount_id, d.discount, d.discount_text
+          FROM discount AS d
+            INNER JOIN customer_discount cd ON d.discount_id = cd.discount_id
+          WHERE customer_id = ?
+        """
+    );
+    result.setInt( 1, customer );
+    return result;
+  }
+
+  private Connection getConnection( String url, String user, String password ) throws SQLException {
+    return DriverManager.getConnection( url, user, password );
+  }
+
+  private Properties getDbAccessProperties() {
+    Properties dbAccessProperties;
+    try( InputStream is = getClass().getClassLoader().getResourceAsStream( "db.properties" ) ) {
+      dbAccessProperties = new Properties();
+      dbAccessProperties.load( is );
+    }
+    catch( IOException e ) {
+      throw new RuntimeException( "database connection properties could not be found" );
+    }
+    return dbAccessProperties;
+  }
+
 }
---
+++ discount/src/main/java/com/wurstbox/atdit/discount/DiscountDB.java
@@ -0,0 +1,3 @@
+package com.wurstbox.atdit.discount;
+
+public record DiscountDB(int discountID, double discount, String discountText) { }
